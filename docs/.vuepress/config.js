358ad1915771b1de5fcd852884071ecde3baf426
liubo <liubo@epipechina.com>
2020-01-16 15:18:04 +0800

    测试提交代码1

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+    base: '/blog-demo/',
+    title: 'blog-demo',
+    description: 'Vuepress blog demo',
+
+    themeConfig: {
+        // 你的GitHub仓库，请正确填写
+        repo: 'https://github.com/tudouxiaoya/blog-demo',
+        // 自定义仓库链接文字。
+        repoLabel: 'My GitHub',
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'FirstBlog', link: '/blog/FirstBlog.md' }
+        ],
+        sidebar: [
+            ['/', '首页'],
+            ['/blog/FirstBlog.md', '我的第一篇博客']
+          ]
+    }
+
+}
